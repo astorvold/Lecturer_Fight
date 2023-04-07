7e3e673730ff602731919cd5d98b7d6c02a1d37f
phg18 <phg18@mscloud.ua.es>
2023-04-07 17:14:44 +0200

    "Finishing the main menu screen design"

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -25,49 +25,19 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import org.w3c.dom.Text;
 
 public class MainMenuScreen implements Screen {
-
-    //Distance between buttons
-    private final int distance = 250;
-
-    Texture avatarButton, titleImage,backgroundImage;
-
-    Preferences prefs = Gdx.app.getPreferences("Lecturer-Fight");
+    private Preferences prefs = Gdx.app.getPreferences("Lecturer-Fight");
     private final float screenHeight = Gdx.graphics.getHeight();
     private final float screenWidth = Gdx.graphics.getWidth();
-
-    private final float buttonWidth = screenWidth/2;
-    private float buttonHeight;
-    SpriteBatch batch = new SpriteBatch();
-    BitmapFont font = new BitmapFont();
-
+    private SpriteBatch batch = new SpriteBatch();
+    private BitmapFont font = new BitmapFont();
     private SettingsScreen settings;
-
-
-
-    final Lecturer_fight game;
-    OrthographicCamera camera;
-
-
-
-    // all the buttons
-
+    private final Lecturer_fight game;
+    private OrthographicCamera camera;
     private final Stage stage;
-    private Texture imageStart;
-    private final ImageButton buttonStart;
-    private Texture imageSettings;
-    private final ImageButton buttonSettings;
-    private Texture imageScore;
-    private final ImageButton buttonScore;
-    private Texture imageTutorial;
-    private final ImageButton buttonTutorial;
-
-    private Texture imageCheckboxOff;
-    private Texture imageCheckboxOn;
-
-    private TextureRegion regionCheckboxOn;
-    private TextureRegion regionCheckboxOff;
-
-    private final CheckBox checkBox;
+    Texture imageStart, imageSettings, imageScore, imageTutorial, imageCheckboxOff, imageCheckboxOn, imageAvatar, titleImage,backgroundImage;
+    private ImageButton buttonStart, buttonSettings, buttonScore, buttonTutorial, buttonAvatar;
+    private TextureRegion regionCheckboxOn, regionCheckboxOff;
+    private CheckBox checkBox;
     private boolean multiplayer;
 
 
@@ -78,11 +48,11 @@ public class MainMenuScreen implements Screen {
         this.settings = new SettingsScreen(game);
         camera.setToOrtho(false,800,400);
 
-        prefs.putString("Avatar", "alfinge_avatar.png");
-        prefs.flush();
+        //prefs.putString("Avatar", "alfinge_avatar.png");
+        //prefs.flush();
 
 
-        avatarButton = new Texture(Gdx.files.internal(prefs.getString("Avatar")));
+        //imageAvatar = new Texture(Gdx.files.internal(prefs.getString("Avatar")));
         titleImage = new Texture(Gdx.files.internal("new_images/TITLE.png"));
         backgroundImage = new Texture(Gdx.files.internal("new_images/BG.png"));
 
@@ -90,38 +60,48 @@ public class MainMenuScreen implements Screen {
         font.getData().setScale(5,5);
         font.setColor(new Color(0x023D8Bff));
 
-        prefs.putInteger("AvatarHeight", 522);
-        prefs.putInteger("AvatarWidth", avatarButton.getWidth()*prefs.getInteger("AvatarHeight")/avatarButton.getHeight());
+        /*prefs.putInteger("AvatarHeight", 522);
+        prefs.putInteger("AvatarWidth", imageAvatar.getWidth()*prefs.getInteger("AvatarHeight")/imageAvatar.getHeight());*/
         prefs.putBoolean("Multiplayer", true);
         prefs.flush();
-
-
         //play music
         System.out.println("musikk er på: " + settings.isMusic_on());
         if (settings.isMusic_on()){
             settings.playMusic();
         }
 
+        initializeButtons();
+        stage = new Stage(new ScreenViewport()); //Set up a stage for the ui
+        stage.addActor(buttonStart); //Add the button to the stage to perform rendering and take input.
+        stage.addActor(buttonSettings);
+        stage.addActor(buttonAvatar);
+        stage.addActor(buttonScore);
+        stage.addActor(buttonTutorial);
+        stage.addActor(checkBox);
+        Gdx.input.setInputProcessor(stage); //Start taking input from the ui
+    }
+
+    private void initializeButtons(){
         imageSettings = new Texture(Gdx.files.internal("new_images/SETTINGS.png"));
         buttonSettings = new ImageButton( new TextureRegionDrawable( new TextureRegion( imageSettings)));
-        buttonSettings.setBounds(buttonSettings.getX(),buttonSettings.getY(),buttonSettings.getWidth(),buttonSettings.getHeight());
-        float screenWidth = Gdx.graphics.getWidth();
-        float screenHeight = Gdx.graphics.getHeight();
-        buttonSettings.setBounds(screenWidth /2 - 0.7f*imageSettings.getWidth()/2, screenHeight *0.9f,imageSettings.getWidth()*0.7f,imageSettings.getHeight()*0.7f);
+        buttonSettings.setBounds(screenWidth*0.1f - 0.5f*imageSettings.getWidth()/2, screenHeight *0.9f,imageSettings.getWidth()*0.5f,imageSettings.getHeight()*0.5f);
+
+        imageAvatar = new Texture(Gdx.files.internal("alfinge_avatar.png"));
+        buttonAvatar = new ImageButton(new TextureRegionDrawable(new TextureRegion(imageAvatar)));
+        buttonAvatar.setBounds(screenWidth*0.9f - 0.5f*imageAvatar.getWidth()/2, screenHeight *0.9f,imageAvatar.getWidth()*0.5f,imageAvatar.getHeight()*0.5f);
 
         imageStart = new Texture(Gdx.files.internal("new_images/PLAY.png"));
         buttonStart = new ImageButton(new TextureRegionDrawable(new TextureRegion(imageStart))); //Set the button up
-        buttonStart.setBounds(screenWidth /2 - 1f*imageStart.getWidth()/2, screenHeight *0.7f,imageStart.getWidth(),imageStart.getHeight());
+        buttonStart.setBounds(screenWidth /2 - 0.7f*imageStart.getWidth()/2, screenHeight *0.6f,0.7f*imageStart.getWidth(),0.7f*imageStart.getHeight());
 
         imageScore = new Texture(Gdx.files.internal("new_images/HIGHSCORE.png"));
         buttonScore = new ImageButton(new TextureRegionDrawable( new TextureRegion(imageScore)));
-        buttonScore.setBounds(screenWidth /2 - 0.7f*imageScore.getWidth()/2, screenHeight *0.3f,imageScore.getWidth()*0.7f,imageScore.getHeight()*0.7f);
+        buttonScore.setBounds(screenWidth /2 - 0.55f*imageScore.getWidth()/2, screenHeight *0.3f,imageScore.getWidth()*0.55f,imageScore.getHeight()*0.55f);
 
 
         imageTutorial = new Texture(Gdx.files.internal("new_images/TUTORIAL.png"));
         buttonTutorial = new ImageButton(new TextureRegionDrawable(new TextureRegion(imageTutorial)));
-        buttonTutorial.setBounds(screenWidth /2 - 0.7f* imageTutorial.getWidth()/2, screenHeight *0.1f, imageTutorial.getWidth()*0.7f, imageTutorial.getHeight()*0.7f);
-
+        buttonTutorial.setBounds(screenWidth /2 - 0.55f* imageTutorial.getWidth()/2, screenHeight *0.15f, imageTutorial.getWidth()*0.55f, imageTutorial.getHeight()*0.55f);
 
         imageCheckboxOn = new Texture(Gdx.files.internal("new_images/TOGGLE_ON.png"));
         imageCheckboxOff = new Texture(Gdx.files.internal("new_images/TOGGLE_OFF.png"));
@@ -131,25 +111,11 @@ public class MainMenuScreen implements Screen {
         font.setColor(Color.WHITE);
         font.getData().setScale(7);
         checkboxStyle.font = font;
-
         checkboxStyle.checkboxOff = new TextureRegionDrawable(regionCheckboxOff);
         checkboxStyle.checkboxOn = new TextureRegionDrawable(regionCheckboxOn);
-
         checkBox = new CheckBox("Multiplayer? ", checkboxStyle);
         checkBox.setBounds(screenWidth /2 - 1f*imageCheckboxOn.getWidth()/2, screenHeight *0.5f,imageCheckboxOn.getWidth(),imageCheckboxOn.getHeight());
-
-        stage = new Stage(new ScreenViewport()); //Set up a stage for the ui
-        stage.addActor(buttonStart); //Add the button to the stage to perform rendering and take input.
-        stage.addActor(buttonSettings);
-        stage.addActor(buttonScore);
-        stage.addActor(buttonTutorial);
-        stage.addActor(checkBox);
-        Gdx.input.setInputProcessor(stage); //Start taking input from the ui
-
-
     }
-
-
     @Override  // Add listeners to check when user clicks on buttons
     public void show() {
        buttonStart.addListener(new ClickListener(){
@@ -166,6 +132,15 @@ public class MainMenuScreen implements Screen {
                 settings.stopMusic();
             }
         });
+
+        buttonAvatar.addListener(new ClickListener(){
+            public void clicked(InputEvent event, float x, float y){
+                game.setScreen(new AvatarScreen(game));
+                System.out.println("Avatar Button");
+                settings.stopMusic();
+            }
+        });
+
         buttonScore.addListener(new ClickListener(){
             public void clicked(InputEvent event, float x, float y){
                 game.setScreen(new HighScoreScreen(game,true,false));
@@ -204,15 +179,14 @@ public class MainMenuScreen implements Screen {
         batch.begin();
         //design
         batch.draw(backgroundImage,0,0,screenWidth,backgroundImage.getHeight()*screenWidth/backgroundImage.getWidth());
-        batch.draw(titleImage,screenWidth/6,screenHeight-250,screenWidth*2/3,titleImage.getHeight()*(screenWidth*2/3)/titleImage.getWidth());
+        batch.draw(titleImage,screenWidth/2 - titleImage.getWidth()/2, 0.73f*screenHeight, titleImage.getWidth(), titleImage.getHeight());
 
         //text
-        font.draw(batch, "Made by Group 16", screenWidth/2-distance*7/6, 150);
-        font.draw(batch, "Multiplayer",(screenWidth-buttonWidth)/2+20,((screenHeight-buttonHeight)/2)+(buttonHeight-distance));
+        font.draw(batch, "Made by Group 16", 0.45f*screenWidth/2, 150);
 
 
         //other buttons
-        batch.draw(avatarButton, (screenWidth-prefs.getInteger("AvatarWidth"))/2, playButtonY+(float)avatarButton.getWidth()/2, prefs.getInteger("AvatarWidth"), prefs.getInteger("AvatarHeight"));
+        //batch.draw(avatarButton, (screenWidth-prefs.getInteger("AvatarWidth"))/2, playButtonY+(float)avatarButton.getWidth()/2, prefs.getInteger("AvatarWidth"), prefs.getInteger("AvatarHeight"));
 
         batch.end();
 
@@ -241,6 +215,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void hide() {
+        batch.dispose();
         stage.dispose();
     }
 
@@ -248,6 +223,7 @@ public class MainMenuScreen implements Screen {
     public void dispose() {
         camera = null;
         stage.dispose();
+        batch.dispose();
 
     }
 
