2085a63f9f0e6b15954adad94ea1fb16acda4ca0
phg18 <phg18@mscloud.ua.es>
2023-04-23 15:55:22 +0200

    "Created the controller class of gameScreen"

---
+++ core/src/com/mygdx/game/GameController.java
@@ -0,0 +1,143 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.ArrayList;
+
+public class GameController {
+
+    public static final int OBSTACLES_PER_SCREEN = 8;
+    public static final int COINS_PER_SCREEN = 3;
+    private final int speed = 3;
+    public Player player;
+    public Player player2;
+    public ArrayList<Entity> obstacles;
+    public ArrayList<Entity> coins;
+    private int highestObstacle;
+    public GameState state;
+    private final boolean multiplayer;
+    private final Lecturer_fight game;
+    private final float screenHeight = Gdx.graphics.getHeight();
+    private final float screenWidth = Gdx.graphics.getWidth();
+
+    public GameController(final Lecturer_fight game, final boolean multiplayer){
+        this.game = game;
+        initializeObstacles();
+        initializeCoins();
+        highestObstacle = 7;
+        this.multiplayer = multiplayer;
+        player = new Player(Configuration.getInstance().getPlayerTexture(), screenWidth/2, screenHeight/2, 96,96);
+        if (Configuration.getInstance().isMusic_on()){
+            Configuration.getInstance().playMusic();
+        }
+        if(!multiplayer){
+            state = GameState.RUNNING_SINGLEPLAYER;
+        }
+        else {
+            player.setReady(true);
+            Texture player2Texture = new Texture("opponent_avatar.png");
+            player2 = new Player(player2Texture, 500, screenHeight/3, 96,96);
+            if(!player2.isReady()) {
+                state = GameState.WAITING;
+            }
+            else {
+                state = GameState.RUNNING_MULTIPLAYER;
+            }
+        }
+    }
+
+    private float generateRandomNumber(int from, int to){
+        return (float)Math.floor(Math.random() * (to - from + 1) + from);
+    }
+
+
+    private void initializeObstacles(){
+        obstacles = new ArrayList<>();
+        for(int i = 1; i <= OBSTACLES_PER_SCREEN; i++) {
+            float aux = screenWidth;
+            int random_int = (int)Math.floor(Math.random() * (1 + 1) + 0);
+            if(random_int == 0) aux = aux * random_int;
+            obstacles.add(new Obstacle(new Texture("obstacles/BUILDING1.png"), aux, screenHeight + screenHeight * (float)(i*1.5)/OBSTACLES_PER_SCREEN, screenWidth*0.3f, screenHeight*0.05f));
+        }
+    }
+    private void initializeCoins(){
+        coins = new ArrayList<>();
+        for(int i = 0; i < COINS_PER_SCREEN; i++){
+            int pos = i*OBSTACLES_PER_SCREEN/COINS_PER_SCREEN;
+            float x = generateRandomNumber(100, (int)screenWidth-100);
+            float y = generateRandomNumber((int)(obstacles.get(pos).getY() + obstacles.get(pos).getHeight()) +70 , (int) obstacles.get(pos).getY()+(64+70));
+            coins.add(new Coin(new Texture("new_images/COIN.png"), x, y, 64,64));
+        }
+    }
+    public void checkCollisions() {
+        //Checks if any obstacle is at the same position that the player
+        for(int i = 0; i < OBSTACLES_PER_SCREEN; i++) {
+            obstacles.get(i).changePos(-speed);
+            if (player.checkColisions(obstacles.get(i))) {
+                //plays die sound if turned on in settings
+                Configuration.getInstance().dieMusic();
+                // send score to DB and set player as non-ready
+                game.api.setScore(player.getScore());
+                player.setReady(false);
+                game.api.setInfoPlayer(player);
+                if(multiplayer == false){
+                    game.setScreen(new HighScoreScreen(this.game,true,true,player.getScore(), 0, null));
+                }else{
+                    if(player2.isAlive()){
+                        game.setScreen(new HighScoreScreen(this.game,true,true,player.getScore(), 999999, player2));
+                    }
+                    else{
+                        game.setScreen(new HighScoreScreen(this.game,true,true,player.getScore(), player2.getScore(),player2 ));
+                    }
+                }
+            }
+            //If the obstacle is getting out the bounds it will be put again
+            if(obstacles.get(i).getY()<0){
+                obstacles.get(i).setY(screenHeight*(float)1.5);
+                highestObstacle=i;
+            }
+        }
+        //Checks if any coin is at the same position that the player
+        for(int i = 0; i < COINS_PER_SCREEN; i++){
+            coins.get(i).changePos(-speed);
+            if (player.checkColisions(coins.get(i))) {
+                player.increaseScore(100);
+                Configuration.getInstance().pointMusic(); //play point sound if it is turned on in settings
+                coins.get(i).disappear();
+            }
+            //If the coin is getting out the bounds it will be put again.
+            //It will be put on top of the highest obstacle
+            if(coins.get(i).getY()<0){
+                float x = generateRandomNumber(100, (int) Gdx.graphics.getWidth()-100);
+                float y = obstacles.get(highestObstacle).getY() + obstacles.get(highestObstacle).getHeight() +70;
+                if(coins.get(i).getTexture() == null){
+                    coins.get(i).setTexture(new Texture("new_images/COIN.png"));
+                }
+                coins.get(i).setX(x);
+                coins.get(i).setY(y);
+            }
+        }
+    }
+
+    public boolean getMultiplayer(){return multiplayer;}
+    public GameState getState(){return state;}
+    public int getSpeed(){return speed;}
+    public Player getPlayer1(){return player;}
+    public Player getPlayer2(){return player2;}
+    public ArrayList<Entity> getObstacles(){return obstacles;}
+    public ArrayList<Entity> getCoins(){return coins;}
+    public void setGameState(GameState state){this.state = state;}
+
+    public void playerController(){
+        if(Gdx.input.getX() >= screenWidth/2) {
+            if (getPlayer1().getX() < screenWidth - getPlayer1().getWidth())
+                getPlayer1().changePos(10);
+            else getPlayer1().changePos(-10);
+        }
+        else {
+            if (getPlayer1().getX() > 0) getPlayer1().changePos(-10);
+            else getPlayer1().changePos(10);
+        }
+    }
+}
--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -18,80 +18,47 @@ import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GameScreen implements Screen{
-    public static final int OBSTACLES_PER_SCREEN = 8;
-    public static final int COINS_PER_SCREEN = 3;
-    public GameState state;
+
     private int backgroundPos = 0;
     private Texture background;
     private Texture player2Texture;
-    private final int speed = 3;
     private final Lecturer_fight game;
     private final float screenHeight = Gdx.graphics.getHeight();
     private final float screenWidth = Gdx.graphics.getWidth();
+
     private OrthographicCamera camera;
     private SpriteBatch batch = new SpriteBatch();
     private BitmapFont font = new BitmapFont();
 
     private SettingsScreen settings;
     private Image buttonPause, buttonResume, buttonQuit;
-    public Player player;
-    public Player player2;
-    public ArrayList<Entity> obstacles;
-    public ArrayList<Coin> coins;
-    private int highestObstacle;
-    long startTime;
-    private boolean multiplayer;
-    private Stage stage;
-    private Stage pausedStage;
     private int player2Score = 0;
     private int player2Score2 = -1;
+    long startTime;
+    private Stage stage;
+    private Stage pausedStage;
+    private GameController gameController;
 
 
-
-
-    public GameScreen(final Lecturer_fight game,boolean multiplayer) {
+    public GameScreen(final Lecturer_fight game, boolean multiplayer) {
         this.game = game;
+        gameController = new GameController(game, multiplayer);
         // create the camera and the SpriteBatch
         this.camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
-
         //Initializing objects
-        initializeObstacles();
-        initializeCoins();
         initializeButtons();
-        highestObstacle = 7;
-        this.multiplayer = multiplayer;
-        player = new Player(Configuration.getInstance().getPlayerTexture(), screenWidth/2, screenHeight/2, 96,96);
-        player2Texture = new Texture("opponent_avatar.png");
-
         //play music
-        if (Configuration.getInstance().isMusic_on()){
-            Configuration.getInstance().playMusic();
-        }
-        if(multiplayer == false){
-            state = GameState.RUNNING_SINGLEPLAYER;
-        }
-        else {
-            player.setReady(true);
-            player2 = new Player(player2Texture, 500, screenHeight/3, 96,96);
-            if(!player2.isReady()) {
-                state = GameState.WAITING;
-            }
-            else {
-                state = GameState.RUNNING_MULTIPLAYER;
-            }
-        }
     }
     private void initializeButtons(){
         Texture imagePause;
-        if(multiplayer == false) {
+        if(!gameController.getMultiplayer()) {
             imagePause  = new Texture("new_images/PAUSE.png");
-            buttonPause = ButtonFactory.createButton(imagePause, screenWidth-imagePause.getWidth()*0.24f, screenHeight - imagePause.getHeight()*0.5f, imagePause.getWidth()*0.2f, imagePause.getHeight()*0.2f);
         }
         else{
             imagePause = new Texture("new_images/QUIT_BOX.png");
-            buttonPause = ButtonFactory.createButton(imagePause, screenWidth-imagePause.getWidth()*0.24f, screenHeight - imagePause.getHeight()*0.5f, imagePause.getWidth()*0.2f, imagePause.getHeight()*0.2f);
         }
+        buttonPause = ButtonFactory.createButton(imagePause, screenWidth-imagePause.getWidth()*0.24f, screenHeight - imagePause.getHeight()*0.5f, imagePause.getWidth()*0.2f, imagePause.getHeight()*0.2f);
 
         Texture imageResume = new Texture("new_images/RESUME.png");
         buttonResume = ButtonFactory.createButton(imageResume,0.5f*screenWidth - 0.6f*screenWidth/2, screenHeight *0.6f,0.6f*screenWidth,0.12f*screenHeight);
@@ -109,54 +76,27 @@ public class GameScreen implements Screen{
         Gdx.input.setInputProcessor(pausedStage);
 
     }
-
-    private float generateRandomNumber(int from, int to){
-        return (float)Math.floor(Math.random() * (to - from + 1) + from);
-    }
-    private void initializeObstacles(){
-        obstacles = new ArrayList<>();
-        for(int i = 1; i <= OBSTACLES_PER_SCREEN; i++) {
-            float aux = screenWidth;
-            int random_int = (int)Math.floor(Math.random() * (1 + 1) + 0);
-            if(random_int == 0) aux = aux * random_int;
-            obstacles.add(new Obstacle(new Texture("obstacles/BUILDING1.png"), aux, screenHeight + screenHeight * (float)(i*1.5)/OBSTACLES_PER_SCREEN, screenWidth*0.3f, screenHeight*0.05f));
-        }
-    }
-    private void initializeCoins(){
-        coins = new ArrayList<>();
-        for(int i = 0; i < COINS_PER_SCREEN; i++){
-            int pos = i*OBSTACLES_PER_SCREEN/COINS_PER_SCREEN;
-            float x = generateRandomNumber(100, (int)screenWidth-100);
-            float y = generateRandomNumber((int)(obstacles.get(pos).getY() + obstacles.get(pos).getHeight()) +70 , (int) obstacles.get(pos).getY()+(64+70));
-            coins.add(new Coin(new Texture("new_images/COIN.png"), x, y, 64,64));
-        }
-    }
     public void movementControl(){
         if (Gdx.input.isTouched()) {
             if((Gdx.input.getX() >= buttonPause.getX() && Gdx.input.getX() <= buttonPause.getX() + buttonPause.getWidth() ) &&
                     ( screenHeight- Gdx.input.getY() >= buttonPause.getY() && screenHeight - Gdx.input.getY() <= buttonPause.getY() + buttonPause.getHeight()) ) {
-                if(state == GameState.RUNNING_SINGLEPLAYER) state = GameState.PAUSED;
-                else if(state == GameState.RUNNING_MULTIPLAYER) game.setScreen(new MainMenuScreen(game));
+                if(gameController.getState() == GameState.RUNNING_SINGLEPLAYER) gameController.setGameState(GameState.PAUSED);
+                else if(gameController.getState() == GameState.RUNNING_MULTIPLAYER) game.setScreen(new MainMenuScreen(game));
             }
-            if(Gdx.input.getX() >= screenWidth/2)
-                if (player.getX() < screenWidth -player.getWidth()) player.changePos(10);
-                else player.changePos(-10);
-            else
-                if (player.getX() > 0) player.changePos(-10);
-                else player.changePos(10);
+            gameController.playerController();
         }
     }
     private void running(){
         batch.begin();
         batch.draw(background,0, backgroundPos, screenWidth,background.getHeight()*screenWidth/background.getWidth());
         batch.end();
-        createEntities();
+        showMap();
         movementControl();
-        checkCollisions();
+        gameController.checkCollisions();
         stage.act(Gdx.graphics.getDeltaTime()); //Perform ui logic
         stage.draw(); //Draw the ui
         if(-backgroundPos >= background.getHeight()) backgroundPos = -3000;
-        else backgroundPos -= speed;
+        else backgroundPos -= gameController.getSpeed();
 
     }
     private void waiting(){
@@ -165,8 +105,8 @@ public class GameScreen implements Screen{
         font.setColor(Color.BLACK);
         font.draw(batch, "Waiting for your opponent!!", Gdx.graphics.getWidth()*0.02f, Gdx.graphics.getHeight()/2);
         batch.end();
-        if(player2.isReady()){
-            state = GameState.RUNNING_MULTIPLAYER;
+        if(gameController.getPlayer2().isReady()){
+            gameController.setGameState(GameState.RUNNING_MULTIPLAYER);
         }
     }
 
@@ -190,19 +130,18 @@ public class GameScreen implements Screen{
         // coordinate system specified by the camera.
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
-
-        switch (state){
+        switch (gameController.getState()){
             case RUNNING_SINGLEPLAYER:
                 running();
                 break;
             case RUNNING_MULTIPLAYER:
-                game.api.getInfoRival(player2);
-                game.api.setInfoPlayer(player);
+                game.api.getInfoRival(gameController.getPlayer2());
+                game.api.setInfoPlayer(gameController.getPlayer1());
                 running();
                 break;
             case WAITING:
-                game.api.setInfoPlayer(player);
-                game.api.getInfoRival(player2);
+                game.api.setInfoPlayer(gameController.getPlayer1());
+                game.api.getInfoRival(gameController.getPlayer2());
                 waiting();
                 break;
             case PAUSED:
@@ -210,40 +149,38 @@ public class GameScreen implements Screen{
                 break;
         }
     }
-    private void createEntities(){
-
+    private void showMap(){
         // begin a new batch and draw the bucket and all drops
         batch.begin();
-        batch.draw(player.getTexture(), player.getX(), player.getY(), player.getWidth(), player.getHeight());
-        for(int i = 0; i < OBSTACLES_PER_SCREEN; i++)
+        batch.draw(gameController.getPlayer1().getTexture(), gameController.getPlayer1().getX(), gameController.getPlayer1().getY(), gameController.getPlayer1().getWidth(), gameController.getPlayer1().getHeight());
+        ArrayList<Entity> obstacles = gameController.getObstacles();
+        for(int i = 0; i < obstacles.size(); i++)
             batch.draw(obstacles.get(i).getTexture(), obstacles.get(i).getX(), obstacles.get(i).getY(), obstacles.get(i).getWidth(), obstacles.get(i).getHeight());
-        for(int i = 0; i < COINS_PER_SCREEN; i++)
+        ArrayList<Entity> coins = gameController.getCoins();
+        for(int i = 0; i < coins.size(); i++)
             if(coins.get(i).getTexture() != null)
                 batch.draw(coins.get(i).getTexture(), coins.get(i).getX(), coins.get(i).getY(), coins.get(i).getWidth(), coins.get(i).getHeight());
         font.getData().setScale(3);
         font.setColor(Color.BLACK);
         // send position to DB
-        game.api.setInfoPlayer(player);
+        game.api.setInfoPlayer(gameController.getPlayer1());
         // player gets 1 point every second
         long elapsedTime = TimeUtils.timeSinceMillis(startTime);
-        if(multiplayer == true){
-            player2Score = player2.getScore();
+        if(gameController.getMultiplayer()){
+            player2Score = gameController.getPlayer2().getScore();
             if (elapsedTime > 1000){
                 if (player2Score == player2Score2){
                     player2Texture = new Texture("opponent_avatar_dead.png");
-                    player2.setTexture(player2Texture);
-                    player2.isDead();
+                    gameController.getPlayer2().setTexture(player2Texture);
+                    gameController.getPlayer2().isDead();
                 }
-                player2Score2 = player2.getScore();
-                player2Score = player2.getScore();
+                player2Score2 = gameController.getPlayer2().getScore();
+                player2Score = gameController.getPlayer2().getScore();
                 startTime = TimeUtils.millis();
+            }
         }
-        }
-
-
-
-        player.increaseScore(10);
-        if (multiplayer == true){
+        gameController.getPlayer1().increaseScore(10);
+        if (gameController.getMultiplayer()){
             //my game gets ready to multiplayer
             showRivalScore();
         }
@@ -251,76 +188,21 @@ public class GameScreen implements Screen{
             showMyScore();
         }
         batch.end();
-
-
     }
     private void showMyScore(){
-        font.draw(batch, "Score: " + player.getScore(), screenWidth*0.45f, screenHeight*0.97f);
+        font.draw(batch, "Score: " + gameController.getPlayer1().getScore(), screenWidth*0.45f, screenHeight*0.97f);
     }
     private void showRivalScore(){
-        if ( player2.isAlive()){
-            batch.draw(player2.getTexture(), player.getX(), player.getY()-100, player2.getWidth(), player2.getHeight());
+        if ( gameController.getPlayer2().isAlive()){
+            batch.draw(gameController.getPlayer2().getTexture(), gameController.getPlayer1().getX(), gameController.getPlayer1().getY()-100, gameController.getPlayer2().getWidth(), gameController.getPlayer2().getHeight());
         }
         else{
-            batch.draw(player2.getTexture(), screenWidth*0.5f, screenHeight*0.1f, player2.getWidth(), player2.getHeight());
+            batch.draw(gameController.getPlayer2().getTexture(), screenWidth*0.5f, screenHeight*0.1f, gameController.getPlayer2().getWidth(), gameController.getPlayer2().getHeight());
         }
-        font.draw(batch, "Player 1: " + player.getScore() + " - Player2: " + player2.getScore(), screenWidth/3, screenHeight*0.97f);
-        game.api.getInfoRival(player2);
+        font.draw(batch, "Player 1: " + gameController.getPlayer1().getScore() + " - Player2: " + gameController.getPlayer2().getScore(), screenWidth/3, screenHeight*0.97f);
+        game.api.getInfoRival(gameController.getPlayer2());
     }
-    public void checkCollisions() {
-        //Checks if any obstacle is at the same position that the player
-        for(int i = 0; i < OBSTACLES_PER_SCREEN; i++) {
-            obstacles.get(i).changePos(-speed);
-            if (player.checkColisions(obstacles.get(i))) {
 
-                //plays die sound if turned on in settings
-                Configuration.getInstance().dieMusic();
-
-                // send score to DB and set player as non-ready
-                game.api.setScore(player.getScore());
-                player.setReady(false);
-                game.api.setInfoPlayer(player);
-                if(multiplayer == false){
-                    game.setScreen(new HighScoreScreen(this.game,true,true,player.getScore(), 0, null));
-
-                }else{
-                    if(player2.isAlive()){
-                        game.setScreen(new HighScoreScreen(this.game,true,true,player.getScore(), 999999, player2));
-                    }
-                    else{
-                        game.setScreen(new HighScoreScreen(this.game,true,true,player.getScore(), player2.getScore(),player2 ));
-                    }
-
-                }
-
-            }
-            //If the obstacle is getting out the bounds it will be put again
-            if(obstacles.get(i).getY()<0){
-                obstacles.get(i).setY(screenHeight*(float)1.5);
-                highestObstacle=i;
-            }
-        }
-        //Checks if any coin is at the same position that the player
-        for(int i = 0; i < COINS_PER_SCREEN; i++){
-            coins.get(i).changePos(-speed);
-            if (player.checkColisions(coins.get(i))) {
-                player.increaseScore(100);
-                Configuration.getInstance().pointMusic(); //play point sound if it is turned on in settings
-                coins.get(i).disappear();
-            }
-            //If the coin is getting out the bounds it will be put again.
-            //It will be put on top of the highest obstacle
-            if(coins.get(i).getY()<0){
-                float x = generateRandomNumber(100, (int)screenWidth-100);
-                float y = obstacles.get(highestObstacle).getY() + obstacles.get(highestObstacle).getHeight() +70;
-                if(coins.get(i).getTexture() == null){
-                    coins.get(i).setTexture(new Texture("new_images/COIN.png"));
-                }
-                coins.get(i).setX(x);
-                coins.get(i).setY(y);
-            }
-        }
-    }
     @Override
     public void resize(int width, int height) {
     }
@@ -331,25 +213,25 @@ public class GameScreen implements Screen{
         startTime = TimeUtils.millis();
         buttonPause.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
-                if(state == GameState.RUNNING_SINGLEPLAYER) state = GameState.PAUSED;
+                if(gameController.getState() == GameState.RUNNING_SINGLEPLAYER) gameController.setGameState(GameState.PAUSED);
                 else game.setScreen(new MainMenuScreen(game));
             }
         });
         buttonQuit.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
-                if(state == GameState.PAUSED) game.setScreen(new MainMenuScreen(game));
+                if(gameController.getState() == GameState.PAUSED) game.setScreen(new MainMenuScreen(game));
             }
         });
         buttonResume.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
-                if(state == GameState.PAUSED) state = GameState.RUNNING_SINGLEPLAYER;
+                if(gameController.getState() == GameState.PAUSED) gameController.setGameState(GameState.RUNNING_SINGLEPLAYER);
             }
         });
     }
     @Override
     public void hide() {
-        player.setReady(false);
-        game.api.removePlayer(player);
+        gameController.getPlayer1().setReady(false);
+        game.api.removePlayer(gameController.getPlayer1());
 
     }
     @Override
@@ -357,11 +239,13 @@ public class GameScreen implements Screen{
     }
     @Override
     public void dispose() {
-        player.getTexture().dispose();
+        gameController.getPlayer1().getTexture().dispose();
         stage.dispose();
         pausedStage.dispose();
         batch.dispose();
-        for(int i = 0; i < OBSTACLES_PER_SCREEN; i++) obstacles.get(i).getTexture().dispose();
-        for(int i = 0; i < COINS_PER_SCREEN; i++) coins.get(i).getTexture().dispose();
+        ArrayList<Entity> obstacles = gameController.getObstacles();
+        ArrayList<Entity> coins = gameController.getCoins();
+        for(int i = 0; i < obstacles.size(); i++) obstacles.get(i).getTexture().dispose();
+        for(int i = 0; i < coins.size(); i++) coins.get(i).getTexture().dispose();
     }
 }
